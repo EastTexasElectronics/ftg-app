9010c7bf98b2b42df4d57174e62111ea6c9d80bd
Robert M Havelaar <Contact@EastTexaselectronics.com>
2024-08-09 09:16:23 -0500

    Initial Commit

---
+++ File Tree Generator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  File Tree Generator
+//
+//  Created by Robert Havelaar on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ File Tree Generator/File_Tree_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  File_Tree_GeneratorApp.swift
+//  File Tree Generator
+//
+//  Created by Robert Havelaar on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct File_Tree_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
