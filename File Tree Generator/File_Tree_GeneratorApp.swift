2b323995aa172ed5a8d86d597639d14e4a1397fe
Robert M Havelaar <Contact@EastTexaselectronics.com>
2024-08-09 18:55:32 -0500

    update

--- File Tree Generator/File_Tree_GeneratorApp.swift
+++ File Tree Generator/File_Tree_GeneratorApp.swift
@@ -1,6 +1,5 @@
 import SwiftUI
 
-/// The main entry point for the File Tree Generator application.
 @main
 struct FileTreeGeneratorApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
@@ -10,42 +9,45 @@ struct FileTreeGeneratorApp: App {
             ContentView()  // The main content view for the application
         }
         .commands {
-            CommandMenu("Help") {
-                // About File Tree Generator Button
+            CommandMenu("About") {
                 Button("About File Tree Generator") {
                     NSApp.orderFrontStandardAboutPanel(nil)  // Opens the standard About panel
                 }
                 .keyboardShortcut("a")  // Keyboard shortcut for the About menu
-                .help("Show information about the File Tree Generator application.")
 
-                // Help Button
                 Button("Help") {
-                    showHelp()  // Opens the Help window
+                    appDelegate.showHelpWindow()
                 }
                 .keyboardShortcut("?")  // Keyboard shortcut for the Help menu
-                .help("Show help information.")
             }
         }
     }
-
-    /// Displays the Help window with the HelpView content.
-    func showHelp() {
-        let helpWindow = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 400, height: 600),
-            styleMask: [.titled, .closable, .resizable],
-            backing: .buffered, defer: false)
-        helpWindow.title = "Help"  // Title of the Help window
-        helpWindow.contentView = NSHostingView(rootView: HelpView())  // Embeds the HelpView in the window
-        helpWindow.makeKeyAndOrderFront(nil)  // Displays the Help window
-    }
 }
 
-/// The application delegate for the File Tree Generator.
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var helpWindow: NSWindow?
 
-    /// Called when the application has finished launching.
-    /// - Parameter notification: The notification indicating the application has finished launching.
     func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.regular)  // Sets the app activation policy to regular
+        NSApp.setActivationPolicy(.regular)
+    }
+
+    @objc func showHelpWindow() {
+        if helpWindow == nil {
+            helpWindow = NSWindow(
+                contentRect: NSRect(x: 0, y: 0, width: 600, height: 600),
+                styleMask: [.titled, .closable, .resizable],
+                backing: .buffered, defer: false)
+            helpWindow?.title = "Help"
+            helpWindow?.contentView = NSHostingView(rootView: HelpView())
+            helpWindow?.center()
+            helpWindow?.makeKeyAndOrderFront(nil)
+        } else {
+            helpWindow?.makeKeyAndOrderFront(nil)
+        }
+    }
+
+    @objc func closeHelpWindow() {
+        helpWindow?.close()
+        helpWindow = nil
     }
 }
